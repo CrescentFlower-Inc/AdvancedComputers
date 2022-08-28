18ef784470afab6241e25ff5cae5ad7036521e6e
HighTally <74025726+HighTally@users.noreply.github.com>
2022-08-28 23:55:47 +0300

    Netweb and Transistor

--- src/main/java/ml/codersquack/ac/core/init/ItemInit.java
+++ src/main/java/ml/codersquack/ac/core/init/ItemInit.java
@@ -11,5 +11,7 @@ public class ItemInit {
   public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AdvancedComputers.MOD_ID);
   public static final RegistryObject<Item> DISK_PLATTER = ITEMS.register("platter",() -> new Item(new Item.Properties().tab(AdvancedComputers.ACITEMS)));
   public static final RegistryObject<Item> DISK_HEAD = ITEMS.register("head",() -> new Item(new Item.Properties().tab(AdvancedComputers.ACITEMS)));
+  public static final RegistryObject<Item> NETWEB = ITEMS.register("netweb",() -> new Item(new Item.Properties().tab(AdvancedComputers.ACITEMS)));
+  public static final RegistryObject<Item> TRANSISTOR = ITEMS.register("transistor",() -> new Item(new Item.Properties().tab(AdvancedComputers.ACITEMS)));
   public static final RegistryObject<Item> ACIG = ITEMS.register("items_group",() -> new Item(new Item.Properties()));
 }
